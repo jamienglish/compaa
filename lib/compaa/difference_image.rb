09e27be01d65e66cf9963e8f1783735d4b645477
Jamie English <jamienglish@gmail.com>
2012-12-08 17:47:08 +0000

    Extracted generated_path into a method

--- lib/compaa/difference_image.rb
+++ lib/compaa/difference_image.rb
@@ -1,29 +1,31 @@
 require 'fileutils'
 
 module Compaa
-	class DifferenceImage < Struct.new(:path)
-		attr_writer :file_manager
+  class DifferenceImage < Struct.new(:path)
+    attr_writer :file_manager
 
-		def create_reference_image
-      generated_path = path.gsub(
-        'differences_in_screenshots_this_run', 'screenshots_generated_this_run'
-      ).gsub('_difference.gif', '')
-
+    def create_reference_image
       file_manager.mkdir_p File.dirname reference_path
       file_manager.cp generated_path, reference_path
       file_manager.rm path
-		end
+    end
 
-		def reference_path
-			path.gsub(
-				'differences_in_screenshots_this_run', 'reference_screenshots'
-			).gsub('_difference.gif', '')
-		end
+    def reference_path
+      path.gsub(
+        'differences_in_screenshots_this_run', 'reference_screenshots'
+      ).gsub('_difference.gif', '')
+    end
 
-		private
+    def generated_path
+      path.gsub(
+        'differences_in_screenshots_this_run', 'screenshots_generated_this_run'
+      ).gsub('_difference.gif', '')
+    end
 
-		def file_manager
-			@file_manager ||= FileUtils
-		end
-	end
+    private
+
+    def file_manager
+      @file_manager ||= FileUtils
+    end
+  end
 end
--- spec/difference_image_spec.rb
+++ spec/difference_image_spec.rb
@@ -6,34 +6,44 @@ require 'compaa'
 
 describe Compaa::DifferenceImage do
   it "creates a reference image" do
-		difference_image_path =
-			File.join %w[artifacts differences_in_screenshots_this_run dir file.png_difference.gif]
+    difference_image_path =
+      File.join %w[artifacts differences_in_screenshots_this_run dir file.png_difference.gif]
 
-		subject = Compaa::DifferenceImage.new difference_image_path
+    subject = Compaa::DifferenceImage.new difference_image_path
 
-		file_manager = MiniTest::Mock.new
-		subject.file_manager = file_manager
+    file_manager = MiniTest::Mock.new
+    subject.file_manager = file_manager
 
-		file_manager.expect :mkdir_p, true, [File.join(%w{artifacts reference_screenshots dir})]
+    file_manager.expect :mkdir_p, true, [File.join(%w{artifacts reference_screenshots dir})]
 
-		file_manager.expect :cp, true,
-			[ File.join(%w[artifacts screenshots_generated_this_run dir file.png]),
-				File.join(%w[artifacts reference_screenshots          dir file.png]) ]
+    file_manager.expect :cp, true,
+      [ File.join(%w[artifacts screenshots_generated_this_run dir file.png]),
+        File.join(%w[artifacts reference_screenshots          dir file.png]) ]
 
-		file_manager.expect :rm, true, [difference_image_path]
+    file_manager.expect :rm, true, [difference_image_path]
 
-		subject.create_reference_image
+    subject.create_reference_image
 
-		file_manager.verify
-	end
+    file_manager.verify
+  end
 
-	it "provides its corresponding reference image path" do
-		path =
-			File.join %w[artifacts differences_in_screenshots_this_run dir file.png_difference.gif]
+  it "provides its corresponding reference image path" do
+    path =
+      File.join %w[artifacts differences_in_screenshots_this_run dir file.png_difference.gif]
 
-		reference_path =
-			File.join %w[artifacts reference_screenshots dir file.png]
+    reference_path =
+      File.join %w[artifacts reference_screenshots dir file.png]
 
-		Compaa::DifferenceImage.new(path).reference_path.must_equal reference_path
-	end
+    Compaa::DifferenceImage.new(path).reference_path.must_equal reference_path
+  end
+
+  it "provides its corresponding generated image path" do
+    path =
+      File.join %w[artifacts differences_in_screenshots_this_run dir file.png_difference.gif]
+
+    generated_path =
+      File.join %w[artifacts screenshots_generated_this_run dir file.png]
+
+    Compaa::DifferenceImage.new(path).generated_path.must_equal generated_path
+  end
 end
