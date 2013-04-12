d01d1cba4f3ed4e1d9666c2e47f4a99f7a087777
Jamie English <jamienglish@gmail.com>
2013-04-12 20:59:14 +0100

    Use a case statement instead of ifelses

--- lib/compaa/rack_app.rb
+++ lib/compaa/rack_app.rb
@@ -15,23 +15,32 @@ module Compaa
         use Rack::Static, :urls => ['/artifacts'], :root => root
 
         run lambda { |env|
-          request = Rack::Request.new env
+          request = Rack::Request.new(env)
 
-          if request.path == '/' and request.params.has_key? 'filepath'
-            locals = { :filepath => request.params['filepath'] }
-            body   = Haml::Engine.new(template).render Object.new, locals
+          case request.path
+          when '/'
+            if request.params.has_key?('filepath')
+              locals = { :filepath => request.params['filepath'] }
+              body   = Haml::Engine.new(template).render Object.new, locals
 
-            [ 200, { 'Content-Type' => 'text/html' }, [body] ]
-          elsif request.path == '/script.js'
-            js = File.read File.expand_path('script.js', File.dirname(__FILE__))
+              [ 200, { 'Content-Type' => 'text/html' }, [body] ]
+            else
+              [ 404, {}, ['Not found'] ]
+            end
+          when '/script.js'
+            js = File.read(File.expand_path('script.js', File.dirname(__FILE__)))
 
             [ 200, { 'Content-Type' => 'application/javascript' }, [js] ]
-          elsif request.path == '/screenshots' and request.request_method == 'POST' and request.params.has_key? 'filepath'
-            generated_image = GeneratedImage.new File.join(root, request.params['filepath'])
-            generated_image.create_reference_image
-            generated_image.delete_difference_image
+          when '/screenshots'
+            if request.request_method == 'POST' && request.params.has_key?('filepath')
+              generated_image = GeneratedImage.new File.join(root, request.params['filepath'])
+              generated_image.create_reference_image
+              generated_image.delete_difference_image
 
-            [ 200, { 'Content-Type' => 'text/plain' }, ['Success'] ]
+              [ 200, { 'Content-Type' => 'text/plain' }, ['Success'] ]
+            else
+              [ 404, {}, ['Not found'] ]
+            end
           else
             [ 404, {}, ['Not found'] ]
           end
