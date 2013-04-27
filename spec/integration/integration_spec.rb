dfacb90c155ee73fd3c338aa8ad242874d668aad
Jamie English <jamienglish@gmail.com>
2013-04-27 18:32:21 +0100

    Made tests consistent with a retry for a couple seconds

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -14,6 +14,19 @@ describe "accepting screenshots from the browser" do
     FileUtils.cp_r('fixtures/homemove', 'tmp/')
   end
 
+  def assert_file_deleted(path)
+    timeout = 2
+    start_time = Time.now
+
+    begin
+      refute File.exists?(path),
+        'Difference image was not deleted'
+    rescue => e
+      retry if (Time.now - start_time) < timeout
+      raise
+    end
+  end
+
   it "Accepts via clicking accept" do
     Dir.chdir 'tmp/homemove' do
       homemove_dir = 'artifacts/differences_in_screenshots_this_run/homemove'
@@ -22,26 +35,23 @@ describe "accepting screenshots from the browser" do
       assert_match %r{#{homemove_dir}/step_2_your_new_home/firefox_Darwin_sky_helpcentre_home_move_your_new_home1\.png_difference\.gif$},
         page.find('img#animation', visible: false)['src']
 
-      click_button 'Accept'
+      click_link 'Accept'
 
-      refute File.exists?("#{homemove_dir}/step_2_your_new_home/firefox_Darwin_sky_helpcentre_home_move_your_new_home1.png_difference.gif"),
-        'Difference image should have been deleted'
+      assert_file_deleted "#{homemove_dir}/step_2_your_new_home/firefox_Darwin_sky_helpcentre_home_move_your_new_home1.png_difference.gif"
 
       assert_match %r{#{homemove_dir}/step_4_contact_details/firefox_Darwin_sky_helpcentre_home_move_contact_details1\.png_difference\.gif$},
         page.find('img#animation', visible: false)['src']
 
-      click_button 'Accept'
+      click_link 'Accept'
 
-      refute File.exists?("#{homemove_dir}/step_4_contact_details/firefox_Darwin_sky_helpcentre_home_move_contact_details1.png_difference.gif"),
-        'Difference image should have been deleted'
+      assert_file_deleted "#{homemove_dir}/step_4_contact_details/firefox_Darwin_sky_helpcentre_home_move_contact_details1.png_difference.gif"
 
       assert_match %r{#{homemove_dir}/validation_failures_on_your_new_home/firefox_Darwin_sky_helpcentre_home_move_your_new_home1\.png_difference\.gif$},
         page.find('img#animation', visible: false)['src']
 
-      click_button 'Accept'
+      click_link 'Accept'
 
-      refute File.exists?("#{homemove_dir}/validation_failures_on_your_new_home/firefox_Darwin_sky_helpcentre_home_move_your_new_home1\.png_difference\.gif"),
-        'Difference image should have been deleted'
+      assert_file_deleted "#{homemove_dir}/validation_failures_on_your_new_home/firefox_Darwin_sky_helpcentre_home_move_your_new_home1\.png_difference\.gif"
 
       assert page.has_selector?('h1', text: 'Done!')
     end
